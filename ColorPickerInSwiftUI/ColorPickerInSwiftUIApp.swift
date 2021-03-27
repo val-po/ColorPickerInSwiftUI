ad3820eb1b859e5ea88be68a253034823e728f9e
VP <VP>
2021-03-27 22:11:21 +0200

    Initial Commit

---
+++ ColorPickerInSwiftUI/ColorPickerInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerInSwiftUIApp.swift
+//  ColorPickerInSwiftUI
+//
+//  Created by valentine on 27.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
